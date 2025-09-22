aa369b0f4975d29b76a0da8b4b9b8b9d3b1a68a5
Adam Sidat <adam@hiddenharvestventures.com>
2025-09-22 16:50:03 -0400

    Fixed import bug

--- validator/main.py
+++ validator/main.py
@@ -14,12 +14,22 @@ from validator.utils.logger import enable_verbose
 from validator.sandbox.sandbox_manager import SandboxManager
 from validator.problem_suites.swebench_verified.swebench_verified_suite import SWEBenchVerifiedSuite
 from validator.config import RIDGES_PROXY_URL
-from validator.problem_suites.swebench_verified.swebench_verified_suite import SWEBENCH_VERIFIED_PROBLEMS
 
 logger = get_logger(__name__)
 
 
 def init_all_images():
+    SWEBENCH_VERIFIED_PROBLEMS = [
+        "astropy__astropy-13398", "astropy__astropy-13579", "django__django-11138",
+        "django__django-11400", "django__django-12325", "django__django-12708",
+        "django__django-13128", "django__django-13212", "django__django-13449",
+        "django__django-13837", "django__django-14007", "django__django-14011",
+        "django__django-14631", "django__django-15268", "django__django-15503",
+        "django__django-15629", "django__django-15957", "django__django-16263",
+        "django__django-16560", "django__django-16631", "pytest-dev__pytest-5787",
+        "pytest-dev__pytest-6197", "pytest-dev__pytest-10356",
+        "sphinx-doc__sphinx-9461", "sphinx-doc__sphinx-11510"
+    ]
     manager = SandboxManager(RIDGES_PROXY_URL)
     swebench_verified_suite = SWEBenchVerifiedSuite(Path(__file__) / "datasets" / "swebench_verified")
     swebench_verified_suite.prebuild_problem_images(manager, SWEBENCH_VERIFIED_PROBLEMS)
