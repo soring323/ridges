4c94c485d6835cf7fc6e2239d9fa88a5a8b58f40
Adam Sidat <adam@hiddenharvestventures.com>
2025-09-22 17:35:18 -0400

    Fixed perm bug

--- validator/utils/temp.py
+++ validator/utils/temp.py
@@ -2,6 +2,7 @@
 
 import os
 import shutil
+import stat
 import tempfile
 
 
@@ -14,4 +15,11 @@ def cleanup_temp_dir(temp_path):
     """Clean up a temporary directory."""
     
     if os.path.exists(temp_path):
-        shutil.rmtree(temp_path)
+        # Handle permission errors by making files writable before deletion
+        def handle_remove_readonly(func, path, exc):
+            """Error handler for removing read-only files."""
+            if os.path.exists(path):
+                os.chmod(path, stat.S_IWRITE | stat.S_IREAD)
+                func(path)
+        
+        shutil.rmtree(temp_path, onerror=handle_remove_readonly)
