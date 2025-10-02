55a2968495147b83d3a50a20f5d7174889e5b5a4
Adam Sidat <adam@hiddenharvestventures.com>
2025-10-02 19:15:38 -0400

    Validator now actually registers

--- validator_2/main.py
+++ validator_2/main.py
@@ -1,4 +1,4 @@
-import httpx
+import time
 import config
 import asyncio
 import utils.logger as logger
@@ -11,15 +11,24 @@ async def main():
     logger.enable_verbose()
 
     try:
-        # Register with the Ridges platform
+        # Get the current timestamp, and sign it with the validator hotkey
+        timestamp = int(time.time())
+        signed_timestamp = config.VALIDATOR_HOTKEY.sign(str(timestamp)).hex()
+
+
+        
+        # Register with the Ridges platform, yielding us a session ID
         logger.info("Registering validator...")
         register_response = await post_ridges_platform('/validator/register', {
-            "signed_timestamp": "FOOBAR",
-            "hotkey": config.VALIDATOR_HOTKEY
+            "timestamp": timestamp,
+            "signed_timestamp": signed_timestamp,
+            "hotkey": config.VALIDATOR_HOTKEY.ss58_address
         })
         session_id = register_response.json()["session_id"]
         logger.info(f"Registered validator. Session ID: {session_id}")
-    
+
+
+
     except Exception as e:
         logger.error(f"Error in main(): {type(e).__name__}: {e}")
         exit(1)
