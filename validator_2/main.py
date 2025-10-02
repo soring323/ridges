95608405a66a465dbe64a5e0c266855c222dc4c5
Adam Sidat <adam@hiddenharvestventures.com>
2025-10-02 17:50:09 -0400

    Added base for validator

--- validator_2/main.py
+++ validator_2/main.py
@@ -1,10 +1,29 @@
+import httpx
 import config
 import asyncio
+import utils.logger as logger
+
+from utils.http import post_ridges_platform
 
 
 
 async def main():
-    pass
+    logger.enable_verbose()
+
+    try:
+        # Register with the Ridges platform
+        logger.info("Registering validator...")
+        register_response = await post_ridges_platform('/validator/register', {
+            "signed_timestamp": "FOOBAR",
+            "hotkey": config.VALIDATOR_HOTKEY
+        })
+        session_id = register_response.json()["session_id"]
+        logger.info(f"Registered validator. Session ID: {session_id}")
+    
+    except Exception as e:
+        logger.error(f"Error in main(): {type(e).__name__}: {e}")
+        exit(1)
+        
 
 
 
@@ -12,4 +31,4 @@ if __name__ == "__main__":
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
-        print("Keyboard interrupt")
+        logger.warn("Keyboard interrupt")
