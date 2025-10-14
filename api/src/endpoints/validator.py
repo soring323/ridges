f2e8d92b87510b4495e5a769464e1173ac7bee05
Adam Sidat <adam@hiddenharvestventures.com>
2025-10-14 17:02:42 -0400

    fix

--- api/src/endpoints/validator.py
+++ api/src/endpoints/validator.py
@@ -632,12 +632,12 @@ async def validator_finish_evaluation(
 
 
 
-    current_evaluation_id = validator.current_evaluation_id
-
-    await handle_evaluation_if_finished(current_evaluation_id)
+    await handle_evaluation_if_finished(validator.current_evaluation_id)
 
     logger.info(f"Validator '{validator.name}' finished an evaluation")
-    logger.info(f"  Evaluation ID: {current_evaluation_id}")
+    logger.info(f"  Evaluation ID: {validator.current_evaluation_id}")
+
+    validator.current_evaluation_id = None
 
     return ValidatorFinishEvaluationResponse()
 
