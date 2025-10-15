fe626697c12f88188d107759ead41bf9189e50f8
Adam Sidat <adam@hiddenharvestventures.com>
2025-10-15 12:18:19 -0400

    fixed configs

--- inference_gateway/config.py
+++ inference_gateway/config.py
@@ -15,24 +15,10 @@ HOST = os.getenv("HOST")
 if not HOST:
     logger.fatal("HOST is not set in .env")
 
-PORT = int(os.getenv("PORT"))
+PORT = os.getenv("PORT")
 if not PORT:
     logger.fatal("PORT is not set in .env")
-
-
-
-# Load Chutes configuration
-CHUTES_BASE_URL = os.getenv("CHUTES_BASE_URL")
-if not CHUTES_BASE_URL:
-    logger.fatal("CHUTES_BASE_URL is not set in .env")
-
-CHUTES_EMBEDDING_URL = os.getenv("CHUTES_EMBEDDING_URL")
-if not CHUTES_EMBEDDING_URL:
-    logger.fatal("CHUTES_EMBEDDING_URL is not set in .env")
-
-CHUTES_API_KEY = os.getenv("CHUTES_API_KEY")
-if not CHUTES_API_KEY:
-    logger.fatal("CHUTES_API_KEY is not set in .env")
+PORT = int(PORT)
 
 
 
@@ -54,9 +40,10 @@ if USE_DATABASE:
     if not DATABASE_HOST:
         logger.fatal("DATABASE_HOST is not set in .env")
         
-    DATABASE_PORT = int(os.getenv("DATABASE_PORT"))
+    DATABASE_PORT = os.getenv("DATABASE_PORT")
     if not DATABASE_PORT:
         logger.fatal("DATABASE_PORT is not set in .env")
+    DATABASE_PORT = int(DATABASE_PORT)
         
     DATABASE_NAME = os.getenv("DATABASE_NAME")
     if not DATABASE_NAME:
@@ -64,10 +51,39 @@ if USE_DATABASE:
 
 
 
+# Load evaluation run configuration
+MAX_INFERENCE_REQUESTS_PER_EVALUATION_RUN = os.getenv("MAX_INFERENCE_REQUESTS_PER_EVALUATION_RUN")
+if not MAX_INFERENCE_REQUESTS_PER_EVALUATION_RUN:
+    logger.fatal("MAX_INFERENCE_REQUESTS_PER_EVALUATION_RUN is not set in .env")
+MAX_INFERENCE_REQUESTS_PER_EVALUATION_RUN = int(MAX_INFERENCE_REQUESTS_PER_EVALUATION_RUN)
+
+
+
+# Load Chutes configuration
+CHUTES_BASE_URL = os.getenv("CHUTES_BASE_URL")
+if not CHUTES_BASE_URL:
+    logger.fatal("CHUTES_BASE_URL is not set in .env")
+
+CHUTES_EMBEDDING_URL = os.getenv("CHUTES_EMBEDDING_URL")
+if not CHUTES_EMBEDDING_URL:
+    logger.fatal("CHUTES_EMBEDDING_URL is not set in .env")
+
+CHUTES_API_KEY = os.getenv("CHUTES_API_KEY")
+if not CHUTES_API_KEY:
+    logger.fatal("CHUTES_API_KEY is not set in .env")
+
+
+
 # Print out the configuration
 logger.info("=== Inference Gateway Configuration ===")
 logger.info(f"Host: {HOST}")
 logger.info(f"Port: {PORT}")
+logger.info("---------------------------------------")
+logger.info(f"Database Username: {DATABASE_USERNAME}")
+logger.info(f"Database Host: {DATABASE_HOST}")
+logger.info(f"Database Port: {DATABASE_PORT}")
+logger.info(f"Database Name: {DATABASE_NAME}")
+logger.info("---------------------------------------")
 logger.info(f"Chutes Base URL: {CHUTES_BASE_URL}")
 logger.info(f"Chutes Embedding URL: {CHUTES_EMBEDDING_URL}")
 logger.info("=======================================")
