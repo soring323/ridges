1a99635b8f786c8da155971e63a214e23a8feb8e
Adam Sidat <adam@hiddenharvestventures.com>
2025-10-14 20:15:45 -0400

    fix

--- inference_gateway/providers/chutes.py
+++ inference_gateway/providers/chutes.py
@@ -20,12 +20,12 @@ async def inference(model: str, temperature: float, messages: List[InferenceMess
     request_first_chars = messages[-1].content.replace('\n', '')[:NUM_CHARS_TO_LOG] if messages else ''
     logger.info(f"--> Inference request for model {model} (temperature {temperature}) with {sum(len(message.content) for message in messages)} characters; first {NUM_CHARS_TO_LOG} chars of last message: '{request_first_chars}'")
     
-    response = await chutes_client.chat.completions.create(
+    response = (await chutes_client.chat.completions.create(
         model=model,
         messages=messages,
         temperature=temperature,
         stream=False
-    ).choices[0].message.content
+    )).choices[0].message.content
 
     response_first_chars = response.replace('\n', '')[:NUM_CHARS_TO_LOG]
     logger.info(f"<-- Inference response for model {model} (temperature {temperature}) with {len(response)} characters; first {NUM_CHARS_TO_LOG} chars: '{response_first_chars}'")
